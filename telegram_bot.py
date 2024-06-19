1dc34193272e0e329874f312061ee937616f0b77
Servaas T <servaas.tilkin@gmail.com>
2024-06-19 15:55:26 +0200

    FIX: removed some demo code

--- telegram_bot.py
+++ telegram_bot.py
@@ -22,16 +22,6 @@ logger = logging.getLogger(__name__)
 
 # Define a few command handlers. These usually take the two arguments update and context.
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
 async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # update.effective_user
     prompt = update.message.text.replace(PROMPT_CMD, '', 1)
@@ -54,6 +44,7 @@ async def generate_image(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     )
     os.remove(local_copy)
 
+
 async def update_config(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     response = handle_config_request(update.message.text, update.effective_user.id)
     await update.message.reply_markdown(response, reply_to_message_id=update.message.message_id)
@@ -67,9 +58,6 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("focus", generate_image))
     application.add_handler(CommandHandler("focus_cfg", update_config))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
