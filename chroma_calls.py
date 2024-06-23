ad7f2f5520208e2fc1807ffe5f1ef7bc3e19e6ea
Servaas T <servaas.tilkin@gmail.com>
2024-06-23 22:00:57 +0200

    ADD: added chromadb

---
+++ chroma_calls.py
@@ -0,0 +1,28 @@
+import chromadb
+
+from fc_settings import all_fc_styles
+
+chroma_client = chromadb.PersistentClient('./chroma/chroma.sqlite')
+
+
+def cdb_insert():
+    keys = []
+    for i in range(len(all_fc_styles)):
+        keys.append(f'{i}')
+
+    collection = chroma_client.get_or_create_collection(name='all_fc_styles')
+    collection.upsert(
+        documents=all_fc_styles,
+        ids=keys
+    )
+
+
+def cdb_query(prompt, n_results=3):
+    collection = chroma_client.get_or_create_collection(name='all_fc_styles')
+    results = collection.query(
+        query_texts=[prompt],  # Chroma will embed this for you
+        n_results=n_results  # how many results to return
+    )
+    return results
+
+# cdb_query('an impressionist painting of megatron')
