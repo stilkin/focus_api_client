a9a70d4fec03d185654089cb842556f7bd46c3c4
Servaas T <servaas.tilkin@gmail.com>
2024-07-06 22:01:49 +0200

    FIX: now properly tags in OpenRouter

--- or_calls.py
+++ or_calls.py
@@ -4,11 +4,10 @@ import os
 import requests
 from dotenv import load_dotenv
 
-from fc_settings import all_fc_styles
-
 load_dotenv()
 TOKEN = os.getenv('OPENROUTER_TOKEN')
-APP_TAG = 'focus-client.be'
+APP_URI = 'https://stilkin.eu'
+APP_TAG = 'Img Prompt Gen'
 MODEL = 'anthropic/claude-3.5-sonnet'
 SYS_PROMPT = (f'You are an assistant that has to work with prompt requests for image generation. '
               'You need to do two things with these prompts. '
@@ -24,8 +23,9 @@ def ask_an_llm(prompt: str, sys_prompt=SYS_PROMPT):
 
     headers = {
         'Authorization': f'Bearer {TOKEN}',
-        'HTTP-Referer': f'{APP_TAG}',
+        'HTTP-Referer': f'{APP_URI}',
         'X-Title': f'{APP_TAG}',
+        'Content-Type': 'application/json'
     }
     data = json.dumps({
         'model': MODEL,
@@ -49,6 +49,7 @@ def expand_prompt(prompt: str):
         if 'message' in choice and 'content' in choice['message']:
             return json.loads(choice['message']['content'])
     return {'prompt': prompt, 'style': None}  # default
+
 
 # resp1 = expand_prompt('a pond like Monet')
 # print(resp1)
